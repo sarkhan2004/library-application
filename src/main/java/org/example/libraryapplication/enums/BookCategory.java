d2cc82ba787c49032a03f2a6d7193cf7aaeeb2d5
sarkhan2004 <Mammadov.Sarkhan.Elkhanovijh.2023@unec.edu.az>
2026-03-07 11:08:13 +0400

    Added enum BookCategory

---
+++ src/main/java/org/example/libraryapplication/enums/BookCategory.java
@@ -0,0 +1,16 @@
+package org.example.libraryapplication.enums;
+
+public enum BookCategory {
+    FICTION,        // Художественная литература
+    NON_FICTION,    // Нехудожественная литература
+    SCIENCE,        // Наука
+    HISTORY,        // История
+    BIOGRAPHY,      // Биография
+    FANTASY,        // Фэнтези
+    SCIENCE_FICTION,// Научная фантастика
+    MYSTERY,        // Детектив
+    ROMANCE,        // Роман
+    HORROR,         // Ужасы
+    EDUCATION,      // Образование
+    BUSINESS        // Бизнес
+}
